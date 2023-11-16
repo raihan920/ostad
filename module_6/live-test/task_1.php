eb8806841273ac80e540dec45967c9778e364bf0
Raihan Chowdhury <raihan92@rocketmail.com>
2023-11-16 15:39:09 +0600

    Working with module 5 assignment

--- module_6/live-test/task_1.php
+++ module_6/live-test/task_1.php
@@ -1,4 +1,4 @@
 <?php
 // Task 1: 
 // Write a SQL query to retrieve all the orders from the 'orders' table and their corresponding customer names from the 'customers' table. Use an INNER JOIN to combine the tables.
-SELECT `orders`.*, `customers`.`name` FROM `orders` INNER JOIN `customers` ON `orders`.`customer_id` = `customers`.`id`
+// SELECT `orders`.*, `customers`.`name` FROM `orders` INNER JOIN `customers` ON `orders`.`customer_id` = `customers`.`id`
--- module_6/live-test/task_2.php
+++ module_6/live-test/task_2.php
@@ -2,4 +2,4 @@
 // Task 2: .
 // Write a SQL query to retrieve all the products from the 'products' table and their corresponding category names from the 'categories' table. Use a LEFT JOIN to combine the tables and include all products, even if they don't have a category.
 
-SELECT `products`.*, `categories`.`name` FROM `products` LEFT JOIN `categories` ON `products`.`category_id` = `categories`.`id` 
+// SELECT `products`.*, `categories`.`name` FROM `products` LEFT JOIN `categories` ON `products`.`category_id` = `categories`.`id` 
